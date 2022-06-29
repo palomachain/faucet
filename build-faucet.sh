cf5c39c153a820202f7c58e728b45bf710cead87
Chase Albert <chase@volume.finance>
2022-06-29 08:59:21 -0600

    Better shell practices in build-faucet.sh

--- build-faucet.sh
+++ build-faucet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -euo pipefail
 
-docker run --rm -v $(pwd):/app golang:1.18 sh -c 'cd /app && GOOS=linux GOARCH=amd64 go build -o build/faucet .'
+exec docker run --rm -v "$(pwd):/app" golang:1.18 sh -c 'cd /app && GOOS=linux GOARCH=amd64 go build -o build/faucet .'
